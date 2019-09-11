05678e76eed502c7f15c1a3f07341fd73decae41
kuna <kgdgwn98@gmail.com>
2019-09-11 22:15:11 +0900

    Now we can hear sound

--- src/Sound.cpp
+++ src/Sound.cpp
@@ -2,6 +2,7 @@
 #include "Game.h"
 #include <iostream>
 #include <algorithm>
+#include "Error.h"
 
 /* rencoder mixer */
 #include "rmixer.h"
@@ -21,7 +22,7 @@ constexpr int kMixerBitsize = 16;
 constexpr int kMixerSampleRate = 44100;
 constexpr int kMixerChannel = 2;
 constexpr size_t kBufferCount = 4;
-constexpr size_t kDefaultMixSize = 4096;
+constexpr size_t kDefaultMixSize = 2048;
 
 ALCdevice *device;
 ALCcontext *context;
@@ -63,11 +64,26 @@ void sound_thread_body()
     while (iBuffersProcessed)
     {
       alSourceUnqueueBuffers(source_id, 1, &cur_buffer_id);
+      memset(pData, 0, buffer_size);
       mixer->Mix(pData, buffer_size);
       alBufferData(cur_buffer_id, audio_fmt, pData, buffer_size, kMixerSampleRate);
       alSourceQueueBuffers(source_id, 1, &cur_buffer_id);
       iBuffersProcessed--;
     }
+
+#if 0
+    ALint state, errcode;
+    alGetSourcei(source_id, AL_SOURCE_STATE, &state);
+    if (state != AL_PLAYING)
+    {
+      ASSERT((errcode = alGetError()) == 0);
+      /**
+       * Actually, unreachable logic. flickering sound.
+       * If reached this logic, then might problem with alSourceQueueBuffers() ...
+       */
+      alSourcePlay(source_id);
+    }
+#endif
 
     _sleep(1);
   }
@@ -88,16 +104,16 @@ void GameMixer::Initialize()
 {
   // get output device from Game settings
   auto& setting = Game::getInstance().getSetting();
+  std::string device_name;
   if (setting.Exist("SoundDevice"))
   {
-    std::string device_name;
     setting.Load("SoundDevice", device_name);
     device = alcOpenDevice(device_name.c_str());
   }
 
   if (!device)
   {
-    std::cerr << "Cannot find sound device or specified device is wrong, use default device." <<
+    std::cerr << "Cannot find sound device or specified device is wrong, use NULL device (warn: no sound)." <<
       std::endl;
     device = alcOpenDevice(0);
     if (!device)
@@ -109,19 +125,19 @@ void GameMixer::Initialize()
 
   // from here, device is must generated.
   context = alcCreateContext(device, 0);
-  alcMakeContextCurrent(context);
+  if (!alcMakeContextCurrent(context))
+  {
+    std::cerr << "Failed to make default context audio" << std::endl;
+  }
 
   alGenSources(1, &source_id);
   alSourcef(source_id, AL_PITCH, 1);
   alSourcef(source_id, AL_GAIN, 1);
   alSource3f(source_id, AL_POSITION, 0, 0, 0);
-  alSource3f(source_id, AL_VELOCITY, 0, 0, 0);
   alSourcei(source_id, AL_LOOPING, AL_FALSE);
 
   alGenBuffers(kBufferCount, buffer_id);
   buffer_size = buffer_size_;
-  sound_thread_finish = false;
-  sound_thread = std::thread(sound_thread_body);
 
   // now mixer initialization
   SoundInfo mixinfo;
@@ -130,9 +146,24 @@ void GameMixer::Initialize()
   mixinfo.channels = kMixerChannel;
   mixer = new Mixer(mixinfo);
 
+  char* pData = (char*)calloc(1, buffer_size);
+  for (int i = 0; i < kBufferCount; ++i)
+  {
+    /* Don't know why but always need to cache dummy data into buffer ... */
+    alBufferData(buffer_id[i], AL_FORMAT_STEREO16, pData, buffer_size, 44100);
+    alSourceQueueBuffers(source_id, 1, &buffer_id[i]);
+    ASSERT(alGetError() == 0);
+  }
+  free(pData);
+
   // okay start stream
-  alSourceQueueBuffers(source_id, 4, buffer_id);
   alSourcePlay(source_id);
+  sound_thread_finish = false;
+  sound_thread = std::thread(sound_thread_body);
+  if (alGetError() != AL_NO_ERROR)
+  {
+    std::cerr << "Something wrong happened playing audio.." << std::endl;
+  }
 }
 
 void GameMixer::Destroy()
@@ -161,7 +192,12 @@ void GameMixer::GetDevices(std::vector<std::string> &names)
   if (enumeration == AL_FALSE)
     return; /* cannot enumerate */
 
-  const ALCchar *devices = alcGetString(NULL, ALC_DEVICE_SPECIFIER);
+  const ALCchar *devices = 0;
+  if (alcIsExtensionPresent(NULL, "ALC_ENUMERATION_ALL_EXT") == AL_FALSE)
+    devices = alcGetString(NULL, ALC_DEVICE_SPECIFIER);
+  else
+    devices = alcGetString(NULL, ALC_ALL_DEVICES_SPECIFIER);
+
   const ALCchar *device = devices, *next = devices + 1;
   size_t len = 0;
 
