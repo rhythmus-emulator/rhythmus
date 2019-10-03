d5f685ec7f3cf4976c354a9c0e5b1cc0f8b8a085
kuna <kgdgwn98@gmail.com>
2019-10-03 21:03:50 +0900

    Added TaskPool for async implementation

---
+++ src/TaskPool.cpp
@@ -0,0 +1,185 @@
+#include "TaskPool.h"
+
+namespace rhythmus
+{
+
+// --------------------------------- class Task
+
+Task::Task()
+  : status_(0), current_thread_(nullptr)
+{
+}
+
+void Task::wait()
+{
+  std::unique_lock<std::mutex> l(mutex_);
+  task_done_cond_.wait(l, [this] { return this->status_ >= 2; });
+}
+
+bool Task::is_started() const
+{
+  return status_ >= 1;
+}
+
+bool Task::is_finished() const
+{
+  return status_ >= 2;
+}
+
+
+//---------------------------- class TaskThread
+
+TaskThread::TaskThread(TaskPool *pool)
+  : is_running_(true), pool_(pool)
+{
+}
+
+TaskThread::TaskThread(TaskThread&& t) noexcept
+{
+  TaskThread::operator=(std::move(t));
+}
+
+TaskThread& TaskThread::operator=(TaskThread&& t) noexcept
+{
+  thread_ = std::move(t.thread_);
+  is_running_ = t.is_running_;
+  current_task_ = std::move(t.current_task_);
+  pool_ = t.pool_;
+  return *this;
+}
+
+TaskThread::~TaskThread()
+{
+  is_running_ = false;
+  abort();
+  if (thread_.joinable())
+    thread_.join();
+}
+
+void TaskThread::run()
+{
+  thread_ = std::thread([this]
+  {
+    for (; is_running_;)
+    {
+      current_task_ = this->pool_->DequeueTask();
+      if (!current_task_)
+        break;
+
+      // mark as running state
+      current_task_->status_ = 1;
+      current_task_->current_thread_ = this;
+
+      // run
+      current_task_->run();
+
+      // mark as finished state
+      current_task_->status_ = 2;
+      current_task_->current_thread_ = nullptr;
+      current_task_->task_done_cond_.notify_all();
+    }
+  });
+}
+
+void TaskThread::set_task(TaskAuto& task)
+{
+  current_task_ = task;
+}
+
+void TaskThread::abort()
+{
+  if (current_task_)
+    current_task_->abort();
+}
+
+// ----------------------------- class TaskPool
+
+TaskPool::TaskPool(size_t size) : pool_size_(0), stop_(false)
+{
+  SetPoolSize(size);
+}
+
+TaskPool::~TaskPool()
+{
+  ClearTaskPool();
+}
+
+void TaskPool::SetPoolSize(size_t size)
+{
+  if (size == 0)
+    return;
+
+  // before re-allocating, cancel all task (if exists)
+  ClearTaskPool();
+
+  for (size_t i = 0; i < size; ++i)
+  {
+    worker_pool_.emplace_back(TaskThread(this));
+    worker_pool_.back().run();
+  }
+
+  pool_size_ = size;
+}
+
+void TaskPool::EnqueueTask(TaskAuto task)
+{
+  std::lock_guard<std::mutex> lock(task_pool_mutex_);
+  task_pool_.push_back(task);
+  task_pool_cond_.notify_one();
+}
+
+TaskAuto TaskPool::DequeueTask()
+{
+  std::unique_lock<std::mutex> lock(task_pool_mutex_);
+  // wait until new task is registered
+  task_pool_cond_.wait(lock,
+    [this] { return this->stop_ || !this->task_pool_.empty(); }
+  );
+  // if thread need to be cleared, exit.
+  if (stop_)
+    return nullptr;
+  // fetch task
+  TaskAuto task = task_pool_.front();
+  task_pool_.pop_front();
+  return task;
+}
+
+void TaskPool::ClearTaskPool()
+{
+  stop_ = true;
+  AbortAllTask();
+  WaitAllTask();
+  worker_pool_.clear();
+  pool_size_ = 0;
+  stop_ = false;
+}
+
+
+void TaskPool::AbortAllTask()
+{
+  task_pool_.clear();
+  for (auto& wthr : worker_pool_)
+    wthr.abort();
+}
+
+void TaskPool::WaitAllTask()
+{
+  while (!task_pool_.empty())
+  {
+    task_pool_.back()->wait();
+  }
+}
+
+bool TaskPool::is_idle() const
+{
+  return task_pool_.empty();
+}
+
+TaskPool& TaskPool::getInstance()
+{
+  static TaskPool pool;
+  return pool;
+}
+
+
+}
---
+++ src/TaskPool.h
@@ -0,0 +1,106 @@
+/**
+ * TaskPool : enqueue / get async tasks using thread pool.
+ * @brief For easy running of async procedures.
+ */
+#pragma once
+
+#include <limits>
+#include <string>
+#include <thread>
+#include <vector>
+#include <list>
+#include <mutex>
+#include <condition_variable>
+
+namespace rhythmus
+{
+
+class TaskPool;
+class TaskThread;
+
+/* @brief A task which is used for execution */
+class Task
+{
+public:
+  // constructor for temporary task
+  Task();
+
+  virtual void run() = 0;
+  virtual void abort() = 0;
+  void wait();
+
+  bool is_started() const;
+  bool is_finished() const;
+
+  friend class TaskPool;
+  friend class TaskThread;
+
+private:
+  /* task status
+   * 0: not run
+   * 1: running
+   * 2: finished
+   */
+  int status_;
+
+  /* owner of this task */
+  TaskThread *current_thread_;
+
+  /* condition variable for task is done */
+  std::condition_variable task_done_cond_;
+  std::mutex mutex_;
+};
+
+using TaskAuto = std::shared_ptr<Task>;
+
+class TaskThread
+{
+public:
+  TaskThread(TaskPool *pool);
+  TaskThread(const TaskThread&) = delete;
+  TaskThread(TaskThread&&) noexcept;
+  TaskThread& operator=(TaskThread&&) noexcept;
+  ~TaskThread();
+  void run();
+  void abort();
+  void set_task(TaskAuto& task);
+
+private:
+  std::thread thread_;
+  bool is_running_;
+  TaskAuto current_task_;
+  TaskPool *pool_;
+};
+
+class TaskPool
+{
+public:
+  TaskPool(size_t size = 0);
+  ~TaskPool();
+
+  void SetPoolSize(size_t size);
+  void ClearTaskPool();
+  void EnqueueTask(TaskAuto task);
+  TaskAuto DequeueTask();
+
+  void AbortAllTask();
+  void WaitAllTask();
+  bool is_idle() const;
+
+  static TaskPool& getInstance();
+
+  friend class TaskThread;
+
+private:
+  size_t pool_size_;
+  std::vector<TaskThread> worker_pool_;
+  bool stop_;
+
+  /* @brief task pool which needs to be retrieved later.
+   * enqueue task with duplicated name in task pool is not allowed. */
+  std::list<TaskAuto> task_pool_;
+  std::mutex task_pool_mutex_;
+  std::condition_variable task_pool_cond_;
+};
+
+}
